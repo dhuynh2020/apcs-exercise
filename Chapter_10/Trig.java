daa15d973efc810ecaac644b6087a311d8f9dc0c
Dylan Huynh <CyKoNinja1999@gmail.com>
2019-08-26 12:54:29 -0600

    Latest work

--- Chapter_10/Trig.java
+++ Chapter_10/Trig.java
@@ -2,8 +2,19 @@ public class Trig
 {
     public static void main (String[] args)
     {
+        final double valuex = 0.5236;
+        final double valuey = 0.5236;
+        Math.sin( valuex );
+        Math.cos( valuey );
+        
+        System.out.println("Sin of " + valuex + " : " + Math.sin(valuex));
+        System.out.println("Cos of " + valuey + " : " + Math.cos(valuey));
+        System.out.println("The Square of Sin " + Math.sin(valuex) + " : " + (Math.sin(valuex) * Math.sin(valuex)));
+        System.out.println("The Square ofC Cos " + Math.cos(valuey) + " : " + (Math.cos(valuey) * Math.cos(valuey)));
+      
         
 
+        
 
 
     }
