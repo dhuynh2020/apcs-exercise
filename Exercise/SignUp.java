9c408a96fffba8cb8108fa174ed2acf332317ed2
Dylan Huynh <CyKoNinja1999@gmail.com>
2019-09-04 13:04:30 -0600

    Signing up

--- Exercise/SignUp.java
+++ Exercise/SignUp.java
@@ -5,5 +5,18 @@ public class Signup
     {
         Scanner scan = new Scanner(System.in);
         
+        System.out.println("admin person jimbob234");
+        String userName=sc.nextLine();
+
+        //Username Checker
+        if(userName.matches("admin person jimbob234"))
+        {
+            System.out.println("Username already taken. Restart if you receive this message.");
+        }
+        else if (userName.matches("skater_gurl117 gary"))
+        {
+            System.out.println("Username is good!. Pasword must start with a vowel, contain a symbol, not have the username in it, and may contain two numbers at the end.");
+
+        }
     }
 }
