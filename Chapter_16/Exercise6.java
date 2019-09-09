f2a83984b1710258b487344925f29f031560aecd
Dylan Huynh <CyKoNinja1999@gmail.com>
2019-09-09 12:54:35 -0600

    Taco Bell

--- Chapter_16/Exercise6.java
+++ Chapter_16/Exercise6.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
-public class Exercise6
-{
-    private static final String e = null;
-
+public class Exercise6{
 	public static void main (String[]args)
     {
         Scanner scan = new Scanner(System.in); 
-        int temp = 50;
-        int speed = 3;
-        double total;
+        int temp;
+        int speed;
+        double windchill;
     
         System.out.println("Input Temperature");
         temp = scan.nextInt();
         System.out.println("Input mph");
         speed = scan.nextInt();
         
-
-
-        if ( 3 <= speed &&  temp >= 50 ){
-            total = 35.74 + (.6215*temp)- 35.75 * ( Math.pow(speed, .16)) + temp*.4275*(Math.pow(speed,.16));
+        if ( speed < 3 || temp > 50)
+        {
+           windchill = temp;
+        } else{
+            windchill = 35.74 + (.6215*temp)- 35.75 * ( Math.pow(speed, .16)) + temp*.4275*(Math.pow(speed,.16));
+            
         }
+        System.out.println("Wind Chill: " +windchill);
     }
 }
