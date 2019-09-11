3188eab7775800566cd4bcc6220541802b651c0f
Dylan Huynh <CyKoNinja1999@gmail.com>
2019-09-11 11:26:54 -0600

    Potatol

--- Chapter_Exercise/Registration.java
+++ Chapter_Exercise/Registration.java
@@ -21,12 +21,19 @@ public class Registration
            
         }
          //Password Checker
-        //  System.out.println("Enter Password:");
-        //  String Password=scan.nextLine();
-        // //  if(Password=matches("x>admin person jimbob");
-        //  {
-        //      System.out.println("");
-        //  }
+       
+
+         System.out.print("Enter a password. Password must start with a vowel, include a symbol, not contain the username, and can only have two numbers at the end.");
+         String pass=scan.nextLine();
+     
+         String lPass=pass.toLowerCase();
+         char first=lPass.charAt(0);
+     
+     if(lPass.matches("^[aeiouy]\\D*?[#!$&]\\D*?\\d{0,2}$") && !lPass.contains(userName.toLowerCase())){
+         System.out.println("It's good!");
+     } else {
+         System.out.println("No good!");
+     }
 
         {
             System.out.println("Good Bye");
--- Chapter_Exercise/TimeExercise.java
+++ Chapter_Exercise/TimeExercise.java
@@ -1,58 +1,93 @@
-
 import java.time.LocalDateTime;
 import java.util.Scanner;
-public class TimeExercise
-{
-    public static void main(String[]args)
-    {
+
+
+public class TimeExercise{
+    public static void main(String[] args){
+         
+        int nowSec=LocalDateTime.now().getSecond();
+        int nowMin=LocalDateTime.now().getMinute();
+        int nowHour=LocalDateTime.now().getHour();
         Scanner scan = new Scanner(System.in);
-        int hour;
-        int minute;
-        int second;
 
-        int a;
-        int b;
-        int d;
+        System.out.println("Enter hour");
+        int hour = Integer.parseInt(scan.nextLine());
 
-        int hrleft;
-        int mileft;
-        int seleft;
-
-
-        System.out.println("Enter Future Hour:");
-        hour = scan.nextInt();
-        System.out.println("Enter Minute");
-        minute = scan.nextInt();
-        System.out.println("Enter Second:");
-        second = scan.nextInt();
-
-        
-
-
-        a = LocalDateTime.now().getHour();
-        b = LocalDateTime.now().getMinute();
-        d = LocalDateTime.now().getSecond();
-
-        if (a >= 12){
-            a = a - 12;
+        while(hour>=24 || hour<nowHour){
+            System.out.print("Invalid hour");
+            hour= Integer.parseInt(scan.nextLine());
         }
-        hrleft = hour -a;
-        mileft = minute - b; 
+
+        System.out.println("Enter minute");
+        int minute = Integer.parseInt(scan.nextLine());
+    
+        while(minute>60 || minute<0 || (minute<nowMin && hour==nowHour)){ //minutes are VALID (0-59) || (minutes>nowMin && hours==nowHours)
+            System.out.print("Invalid minute");
+            minute= Integer.parseInt(scan.nextLine());
+        } 
+
+        System.out.println("Enter second");
+        int second = Integer.parseInt(scan.nextLine());
+            
+        while(second>60 || second<0 || (second<=nowSec && minute==nowMin && hour==nowHour)){
+            System.out.print("Invalid second");
+            second= Integer.parseInt(scan.nextLine());
+        }
         
-        seleft = second -  d; 
-        //if seleft()
-
-
-       System.out.println("You have " + Math.abs(hrleft) + " hours left.");
-       System.out.println("You have " + Math.abs(mileft) + " minutes left.");
-       System.out.println("You have " + Math.abs(seleft) + " seconds left.");
-
- 
-        
-        LocalDateTime time = LocalDateTime.now();
-        System.out.println(time);
+        int newHour= hour-nowHour;
+        int newMin= minute-nowMin;
+        int newSec= second-nowSec; 
         
        
+        //-59<0  so: newHour-=1; newMin+=60;
+        if(newMin<0){ //adjust minutes
+            newHour-=1;
+            newMin+=60;
+        }
+        
+        //-58<0  so: newMin-=1; newSec+=60;
+        if(newSec<0){ //adjust seconds
+            newMin-=1;
+            newSec+=60;
+        }
+        
+        String result="";
+        if(newHour>0){
+            result+=newHour;
+            result+=" hour";
+            if(newHour>1)
+                result+="s";
+        }
+        result+=" ";
+
+        String result1="";
+        if(newMin>0){
+            result1+=newMin;
+            result1+=" Minute";
+            if(newMin>1)
+                result1+="s";
+        }
+        result1+=" ";
+
+        String result2="";
+        if(newSec>0){
+            result2+=newSec;
+            result2+=" Second";
+            if(newSec>1)
+                result2+="s";
+        }
+        result1+=" ";
+            //add something for hours
+        //check minutes
+            //add something for minutes
+        //check seconds
+            //add something for seconds
+            
+        result=result.trim();
+     
+        
+        
+        System.out.format("Given %2d:%2d:%2d at %2d:%2d:%2d->\t%s %s %s",hour,minute,second,nowHour,nowMin,nowSec,result,result1, result2);
     }
-}
     
+}
