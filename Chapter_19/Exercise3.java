84e3513053fb61a5d1e72b4f003113b64dfaa912
Dylan Huynh <CyKoNinja1999@gmail.com>
2019-09-15 22:36:28 -0600

    Homework 23

--- Chapter_19/Exercise3.java
+++ Chapter_19/Exercise3.java
@@ -8,12 +8,14 @@ public class Exercise3
         String word;
         String word2;
         int space = 0;
-        String period = System.out.println(".");
+        String period;
+
+
         System.out.println("Enter first word");
         word = scan.nextLine();
         System.out.println("Enter second word");
         word2 = scan.nextLine();
-        space = space + 1;
+        
         while(space <= 30){
             System.out.println(period);
             space = space + 1; 
