25e2bb9924f41f3e27a012cb3f58a5b96f03d935
Dylan Huynh <CyKoNinja1999@gmail.com>
2019-10-01 12:53:55 -0600

    hi

--- Chapter_30/GuessingGame.java
+++ Chapter_30/GuessingGame.java
@@ -4,15 +4,21 @@ public class GuessingGame{
     public static void main(String[]args)
     {
         Scanner scan = new Scanner(System.in);
-        Random x = new Random(11);
+        Random rand = new Random();
+        int x = rand.nextInt(10) + 1;
         int y;
-        System.out.println("I am thinking of a number from 1 to 10. \n You must guess what it is in three tries. \n Enter a guess: ");
+        int tries = 3;
+        System.out.println("I am thinking of a number from 1 to 10.\nYou must guess what it is in three tries.\nEnter a guess: ");
         y = scan.nextInt();
-
-        if (y == x ){
+        
+        while (tries != 1){
+            tries = tries - 1;
+            System.out.println("Incorrect Try Again you have " + tries + " Left");
+            y = scan.nextInt();
+        } if (y == x ){
         System.out.println("RIGHT! \n You have won the game.");
-        } else if( y == x){
-
+        } else {
+            System.out.println("The correct answer was " + x + "\nYou have lost the game." );
         }
     }
 }
---
+++ Chapter_30/PasswordCracker.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class PasswordCracker
+{
+    public static void main(String[]args)
+    {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        int digits = 0;
+
+        System.out.Println("Enter Password (5 or less character)");
+        while (digits < 5) {
+        digit = rand    
+        }
+
+    }
+}
