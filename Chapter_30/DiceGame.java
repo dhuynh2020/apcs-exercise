07a8707844e6a76c91f04e258ead5610ab4511e9
Dylan Huynh <CyKoNinja1999@gmail.com>
2019-10-08 12:52:38 -0600

    Save File 1

--- Chapter_30/DiceGame.java
+++ Chapter_30/DiceGame.java
@@ -3,6 +3,28 @@ import java.util.*;
 public class DiceGame{
     public static void main(String[]args)
     {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random() ;
+
+        String firstPlayer = "";
+        String secondPlayer = "";
+        int sum; 
+        double firstPlayerDiff, secondPlayerDiff; 
+
+
+        System.out.println()
+
+        boolean endGame = false
         
+        while(!endgame){
+            sum = Math.random() * 6 + 1;
+            x = x + sum;  
+        } if (sum == 1){
+            endGame = true; 
+
+            
+        } else {
+
+        }
     }
 }
