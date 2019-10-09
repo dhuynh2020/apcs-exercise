12491edcc3f4ac5f4c6f28b5607e004f0dcdfa8a
Dylan Huynh <CyKoNinja1999@gmail.com>
2019-10-09 13:12:57 -0600

    Thomas is ded to me

--- Chapter_30/DiceGame.java
+++ Chapter_30/DiceGame.java
@@ -5,26 +5,47 @@ public class DiceGame{
     {
         Scanner scan = new Scanner(System.in);
         Random rand = new Random() ;
-
-        String firstPlayer = "";
-        String secondPlayer = "";
+        int x; 
         int sum; 
-        double firstPlayerDiff, secondPlayerDiff; 
+        int playerOnePoints = 0;
+        int computerScore = 0;
 
-
-        System.out.println()
-
-        boolean endGame = false
         
-        while(!endgame){
-            sum = Math.random() * 6 + 1;
-            x = x + sum;  
-        } if (sum == 1){
-            endGame = true; 
+         
 
+
+        System.out.println("You are playing the game Pigy. Each player starts at 0 points,\nwhoever gets to 100 Wins.");
+
+        boolean endGame = false;
+        
+        while(!endGame){
             
+            int sum1 = (int)(Math.random() * 6 + 1);
+            int sum2 = (int)(Math.random() * 6 + 1);
+            int sum3 = (int)(Math.random() * 6 + 1);
+            int sum4 = (int)(Math.random() * 6 + 1);
+            int sum5 = (int)(Math.random() * 6 + 1);
+            int sum6 = (int)(Math.random() * 6 + 1);
+             
+        computerScore = computerScore + sum1 + sum2 + sum3;
+          if (sum4 == 1 || sum5 == 1 || sum6 == 1){
+            playerOnePoints = playerOnePoints;
+  
         } else {
+            playerOnePoints = playerOnePoints + sum4 + sum5 + sum6;
 
+        }
+        System.out.println("The Computer's Score is: " + computerScore);
+        System.out.println("The Player's score is: " + playerOnePoints);
+         if (playerOnePoints >=100 || computerScore >= 100){
+            endGame = true;
+        } 
+        
+         if (playerOnePoints >= 100){
+            System.out.println("Player One Wins!!!");
+        }else {
+            System.out.println("Computer Wins!!!");
+        }
         }
     }
 }
