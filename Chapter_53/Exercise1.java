8f8f30803ea84b136dfad683b7db088b77a11707
Dylan Huynh <CyKoNinja1999@gmail.com>
2019-11-06 13:10:18 -0700

    PokeMon

---
+++ Chapter_53/Exercise1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Exercise1
+{
+    public static void main (Strings[]args) 
+    {
+        Scan scan = new Scanner( System.in);
+        String inChars;
+        String str = new String();
+       
+        int spread; 
+        Jam goose = new Jam ( "Gooseberry", " 7/4/86", 12);
+        Jam apple = new Jam ( "Crab Apple", " 9/30/99", 8);
+        Jam rhub = new Jam ( "Rhubarb", " 10/31/99", 16);
+
+        System.out.println(goose);
+        System.out.println(apple);
+        System.out.println(rhub);
+
+        System.out.println("Enter yor selection (1, 2, or 3)");
+     
+        System.out.println("Enter amount to spread: ");
+
+        System.out.println("Spreading  " + );
+
+
+    }
+}
