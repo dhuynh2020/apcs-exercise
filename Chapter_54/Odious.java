b4100ab84f95ad308d96001685194ea3d7325d01
Dylan Huynh <CyKoNinja1999@gmail.com>
2019-11-22 12:53:24 -0700

    In Progress

--- Chapter_54/Odious.java
+++ Chapter_54/Odious.java
@@ -11,17 +11,35 @@ public class Odious{
     int usrInput = scan.nextInt();
     
     int remainder = usrInput % 2; 
-    int dividend = usrInput / 2;
+    int remainder2 = 0;
+    int dividend = usrInput ;
     String newRep;
     String binaryRep = "";
+    System.out.println();
     while ( dividend != 0) {
         remainder = dividend % 2;
         dividend = dividend / 2;
+        remainder2 = remainder + remainder; 
+        // System.out.println(remainder);
+        // System.out.println(remainder2);
+        // System.out.println();
+        
+        // System.out.println(dividend);
         newRep = binaryRep + remainder; 
         System.out.println(newRep); 
+
         // System.out.println( "Remainder          Dividend\n" + remainder + "          " + dividend);
     }
-
+    
+    while (Math.round(remainder2) >= 2){
+      remainder2 = remainder2 / 2; 
+    }
+    // remainder2 = remainder2 - 1; 
+    if (remainder2 == 1){
+      System.out.println("The number you were given is an Odious Number");
+    } else if(remainder2 == 0){ 
+      System.out.println("The number you were given is an Evil Number");
+    }
     
 
 
