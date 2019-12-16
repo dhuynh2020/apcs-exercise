5f3f9bc4447b6339712b995b7f309d89d73189ca
Dylan Huynh <CyKoNinja1999@gmail.com>
2019-12-16 12:44:04 -0700

    Redid credit

--- Chapter_23/Exercise4.java
+++ Chapter_23/Exercise4.java
@@ -2,12 +2,30 @@ import java.util.Scanner;
 public class Exercise4
 {
     public static void main(String[]args){
-        Scanner scan = new Scanner ( System.in );
-        int x;
+        int n = 0; 
+        int x = 0;
+        Scanner scan = new Scanner(System.in);
+        double sum = 0.0;
+        double term = 1; 
 
-        System.out.println("Enter X: ");
+        System.out.println("Enter Sum:");
         x = scan.nextInt();
+        while ( term > 1.0e-12) { 
+            term = Math.pow(x, n) / factorial(n);
+            sum += term;
+            n++;
+        }
+        System.out.println("My: " + sum);
+        System.out.println("Actual: " + Math.exp(x)); 
+    }
 
-        System.out.println(Math.exp(x));
+    public static double factorial(double n) {
+        int i = 1; 
+        double factorial = 1;
+        while( i <= n){
+            factorial *= i;
+            i++;
+        }
+        return factorial;
     }
 }
